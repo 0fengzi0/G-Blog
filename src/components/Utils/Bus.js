5f37b5bd50dc7f32caedef927e013e2afe59d052
疯子 <1209711408@qq.com>
2020-06-09 16:45:32 +0800

    新增了cookie相关 新增了axios封装

---
+++ src/components/Utils/Bus.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+const Bus = new Vue();
+
+export default Bus;
+
+//Bus.$emit("name",data)
+//Bus.$on("name",fun(){})
---
+++ src/components/Utils/HttpClient.js
@@ -0,0 +1,88 @@
+import axios from "axios"
+import qs from 'qs';
+import Bus from "./Bus";
+
+// 统一异常处理部分
+function errorHandle(res) {
+    switch ( res.code ) {
+        case 404:
+            res.msg = "404文件不存在"
+            break;
+    }
+    res.msg == null ? res.msg = "网络请求错误" : '';
+    // Bus.$toast.fail(res.msg);
+}
+
+// 创建axios实例
+const instance = axios.create({
+    // 超时 15秒
+    timeout: 1000 * 15,
+    // 请求头
+    headers: {
+        'Content-Type': "application/x-www-form-urlencoded"
+        
+    }
+});
+
+// 请求拦截器
+instance.interceptors.request.use(res => {
+    if ( res.method === "post" || res.method === "POST" ) {
+        res.data.time = new Date().getTime();
+        res.data.sessionid = Bus.$cookies.get('sessionid') == null ? '' : Bus.$cookies.get('sessionid');
+        res.data = qs.stringify(res.data);
+        return res;
+    } else if ( res.method === "get" || res.method === "GET" ) {
+        res.params.time = new Date().getTime();
+        res.params.sessionid = Bus.$cookies.get('sessionid') == null ? '' : Bus.$cookies.get('sessionid');
+        return res;
+    }
+});
+
+// 响应拦截器
+instance.interceptors.response.use(res => {
+    Bus.$toast.clear();
+    if ( res.data.sessionid != null ) {
+        Bus.$cookies.set('sessionid', res.data.sessionid)
+    }
+    if ( res.data.code !== 200 ) {
+        errorHandle(res.data);
+        return Promise.reject(res.data);
+    }
+    return Promise.resolve(res.data);
+}, error => {
+    Bus.$toast.clear();
+    let data = {
+        code: error.response.status,
+        msg: error.message,
+        data: error
+    };
+    errorHandle(data);
+    return Promise.reject(data);
+});
+
+// 设置服务器地址,开发环境用
+let serviceHost = (process.env.NODE_ENV === 'production' || process.env.VUE_APP_HOST == null) ? 'https://127.0.0.1/' : process.env.VUE_APP_HOST;
+
+
+function doHttp(url = "", type = "get", data = {}) {
+    Bus.$toast.loading({
+        message: '加载中...',
+        forbidClick: true,
+        duration: 0
+    });
+    if ( type === "get" || type === "GET" ) {
+        return instance.get(serviceHost + url, {
+            params: data
+        })
+    } else if ( type === "post" || type === "POST" ) {
+        return instance.post(serviceHost + url,
+            data
+        )
+    }
+}
+
+export default {
+    doHttp,
+    serviceHost
+}
+
